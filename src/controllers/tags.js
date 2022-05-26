6dff27dc1d264f37d77a211abd32418df97038bf
Devesh Patel <deveshpatel13599@gmail.com>
2022-05-26 10:43:11 -0400

    get all tags in the first request

--- src/controllers/tags.js
+++ src/controllers/tags.js
@@ -2,7 +2,7 @@ import { validateResponse } from '../utils/validateResponse';
 
 const URL = 'https://dp-expense-manager-api.herokuapp.com';
 
-export const getTags = async (page = 1, perPage = 10) => {
+export const getTags = async (page = 1, perPage = 100) => {
     const response = await fetch(
         `${URL}/tags?page=${page}&perPage=${perPage}`,
         {
